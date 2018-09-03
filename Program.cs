fe9d9d59e89f0a5ca2482fe957eb5be1733dae0d
Martin Fryd Wochner <martin.wochner@gmail.com>
2018-09-03 16:02:21 +0200

    Dynamic FizzBuzz
    Added a Dynamic FizzBuzz ruleset

--- Program.cs
+++ Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace FizzBuzzGame
 {
     class Program
     {
-        static string[] cheatsheet = File.ReadAllLines("prediction.txt");
+        private static readonly string[] Cheatsheet = File.ReadAllLines("prediction.txt");
+
+        public static bool IsCorrect(string result, int number)
+        {
+            var line = Cheatsheet.GetValue(number).ToString().Split(':');
+            return string.Equals(result, line[1].Trim(), StringComparison.CurrentCultureIgnoreCase);
+        }
+
         static void Main(string[] args)
         {
+            var game1 = new FizzBuzzDynamic(Enumerable.Range(1, 100), new[]
+            {
+                (3, "Fizz"),
+                (5, "Buzz"),
+                (7, "Woof")
+            });
 
-            for (int i = 1; i <= 100; i++)
+            var game2 = new FizzBuzzStatic();
+
+            PlayFizzBuzz(game1);
+            PlayFizzBuzz(game2);
+        }
+
+        public static void PlayFizzBuzz(IFizzBuzzGame game)
+        {
+            var results = game.GetFizzBuzz().ToList();
+
+            for (var i = 0; i < results.Count; i++)
+            {
+                Print(results[i], IsCorrect(results[i], i) ? ConsoleColor.Green : ConsoleColor.Red);
+            }
+        }
+
+        static void Print(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+
+    }
+
+    interface IFizzBuzzGame
+    {
+        IEnumerable<string> GetFizzBuzz();
+    }
+
+    class FizzBuzzStatic : IFizzBuzzGame
+    {
+        public IEnumerable<string> GetFizzBuzz()
+        {
+            for (var i = 1; i <= 100; i++)
             {
 
                 var result = "";
@@ -19,32 +69,36 @@ namespace FizzBuzzGame
                 var woof = (i % 7 == 0);
 
                 if (fizz || buzz || woof) result
-               = (fizz == true ? "Fizz" : "")
-               + (buzz ? "Buzz" : "")
-               + (woof ? "Woof" : "");
+                    = (fizz ? "Fizz" : "")
+                      + (buzz ? "Buzz" : "")
+                      + (woof ? "Woof" : "");
                 else result = i.ToString();
 
-                if (!IsCorrect(result, i))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(result);
-                    Console.ResetColor();
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(result);
-                    Console.ResetColor();
-                }
+                yield return result;
 
             }
         }
+    }
 
-        static public bool IsCorrect(string result, int number)
+    class FizzBuzzDynamic : IFizzBuzzGame
+    {
+        private readonly IEnumerable<int> _sequence;
+        private readonly (int Number, string Word)[] _triggers;
+        public FizzBuzzDynamic(IEnumerable<int> sequence, (int,string)[] triggers)
         {
-            var line = cheatsheet.GetValue(number).ToString().Split(':');
-            return result.ToUpper() == line[1].Trim().ToUpper();
+            _sequence = sequence;
+            _triggers = triggers;
         }
 
+        public IEnumerable<string> GetFizzBuzz()
+        {
+            foreach (var i in _sequence)
+            {
+                var result = _triggers.Where(trigger => i % trigger.Number == 0).Aggregate("", (current, trigger) => current + trigger.Word);
+
+                if (string.IsNullOrEmpty(result)) result = i.ToString();
+                yield return result;
+            }
+        }
     }
 }
